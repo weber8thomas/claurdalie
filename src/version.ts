26ecaf508b854263754592ed9c0240587aa912f8
weber8thomas <weber8thomas@gmail.com>
2026-07-03 18:37:44 +0000

    chore: release v0.6.0 (drop -beta suffix)

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Keep in sync with package.json "version".
-export const APP_VERSION = '0.6.0-beta.0'
+export const APP_VERSION = '0.6.0'
 export const REPO_URL = 'https://github.com/weber8thomas/claurdalie'
